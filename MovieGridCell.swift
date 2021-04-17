cfdd35119f9dcb4979d84ac84c01ca288b6794e8
Erin DeLong <erindelong@umail.ucsb.edu>
2021-04-16 21:13:23 -0700

    week 2

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Movie App
+//
+//  Created by Erin DeLong on 4/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
